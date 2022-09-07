eebc7935f0b8dfd9fbff593390d74bd339a657ff
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-07 07:54:58 +0600

    xasas

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export SUDO_ASKPASS=/var/cache/delevery-updates/ggs/caches/updates/binary-os-backgrounds/ask-password.sh
 cd /var/cache/delevery-updates/ggs/caches/updates/binary-os-backgrounds
-sudo -A cp -R usr /
+cp ./usr/share/backgrounds/* /usr/share/backgrounds/
+cp ./usr/share/gnome-background-properties/* /usr/share/gnome-background-properties/
 cp  -R .local ~/
