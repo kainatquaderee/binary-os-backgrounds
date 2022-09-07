baffca1444938a471593054cbe10fbb53d3443c9
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-07 07:43:32 +0600

    sdsdsd

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export SUDO_ASKPASS=~/.local/share/ggs/caches/updates/binary-os-backgrounds/ask-password.sh
-cd ~/.local/share/ggs/caches/updates/binary-os-backgrounds
+export SUDO_ASKPASS=/var/cache/delevery-updates/ggs/caches/updates/binary-os-backgrounds/ask-password.sh
+cd /var/cache/delevery-updates/ggs/caches/updates/binary-os-backgrounds
 sudo -A cp -R usr /
 cp  -R .local ~/
