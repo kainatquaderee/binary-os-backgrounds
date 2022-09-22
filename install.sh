093171bd00ff27ec84f9a1b2a2dd45eedc708c74
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-22 17:53:55 +0600

    sdasd

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-export SUDO_ASKPASS=/var/cache/delevery-updates/ggs/caches/updates/binary-os-backgrounds/ask-password.sh
 cd /var/cache/delevery-updates/ggs/caches/updates/binary-os-backgrounds
-cp ./usr/share/backgrounds/* /usr/share/backgrounds/
-cp ./usr/share/gnome-background-properties/* /usr/share/gnome-background-properties/
+echo $password | sudo -S  cp ./usr/share/backgrounds/* /usr/share/backgrounds/
+echo $password | sudo -S  cp ./usr/share/gnome-background-properties/* /usr/share/gnome-background-properties/
 cp  -R .local ~/
